13ae98c222c4dc2456af189ae705ce16bdd1bf47
forsejy <andrij200198@gmail.com>
2021-01-20 14:46:56 +0200

    angular first

---
+++ src/app/database/db.ts
@@ -0,0 +1,7 @@
+export const cars = [
+  {producer: 'AUDI', model: 'A7', price: 9999, power: 250},
+  {producer: 'AUDI', model: 'A6', price: 8888, power: 190},
+  {producer: 'BMW', model: 'M5', price: 99999, power: 700},
+  {producer: 'LADA', model: '05', price: 99, power: 90},
+  {producer: 'OPEL', model: 'Astra', price: 999, power: 100},
+];
